fb22d22a96dfc1cd1b2ae5e813a04fdd4d6e4033
valegaru <valegaru2003@gmail.com>
2023-10-24 21:21:10 -0500

    registroEinicioLinkeadosLocalStorageFuncional

--- 4Inicio-sesion.js
+++ 4Inicio-sesion.js
@@ -4,43 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault();
 
-        // Obtén el valor del campo de entrada
         const emailOrUsuario = document.getElementById("emailOrUser").value;
         const password = document.getElementById("password").value;
 
-        let email;
-        let usuario;
+        // Comprobar si se ha registrado con email
+        let storedEmail = localStorage.getItem("email");
+        let storedUsuario = localStorage.getItem("usuario");
 
-        // Verifica si el valor contiene "@" para determinar si es un correo electrónico o usuario
-        if (emailOrUsuario.includes("@")) {
-            // Es un correo electrónico
-            email = emailOrUsuario;
-        } else {
-            // No contiene "@", se asume como un usuario
-            usuario = emailOrUsuario;
-        }
+        if (emailOrUsuario === storedEmail || emailOrUsuario === storedUsuario) {
+            let storedPassword = localStorage.getItem("contrasena");
+            
+            if (password === storedPassword) {
+                // Las credenciales son correctas
 
-        // Verifica las credenciales
-        if ((email === "tiago123456@gmail.com" || usuario === "tiago123") && password === "12345678") {
-            if (email === "tiago123456@gmail.com") {
-                // Guarda el correo electrónico en localStorage
-                localStorage.setItem("email", email);
-                localStorage.setItem("password", password);
+                // Limpia los campos de entrada
+                document.getElementById("emailOrUser").value = "";
+                document.getElementById("password").value = "";
+
+                // Redirige al usuario a "7holadenuevo.html"
+                window.location.href = "./7holadenuevo.html";
             } else {
-                // Guarda el usuario en localStorage
-                localStorage.setItem("usuario", usuario);
-                localStorage.setItem("password", password);
+                // Contraseña incorrecta
+                alert("Contraseña incorrecta. Por favor, verifica tu contraseña.");
             }
-
-            // Limpia los campos de entrada
-            document.getElementById("emailOrUser").value = "";
-            document.getElementById("password").value = "";
-
-            // Redirige al usuario a "7holadenuevo.html"
-            window.location.href = "./7holadenuevo.html";
         } else {
-            // Si las credenciales no coinciden, muestra un mensaje de error
-            alert("Credenciales incorrectas. Por favor, verifica tu usuario y contraseña.");
+            // Email o Usuario incorrecto
+            alert("Email o Usuario incorrecto. Por favor, verifica tus credenciales.");
         }
     });
 });
