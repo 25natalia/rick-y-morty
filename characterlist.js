6bb41fb362cf4c810371e99cf5170bd5ccc8fb56
kikipou <kta2122006@gmail.com>
2023-10-26 13:55:58 -0500

    Update characterlist.js

--- characterlist.js
+++ characterlist.js
@@ -1,7 +1,8 @@
 class Characters {
 
     constructor(id, name, status, species, type, gender, origin, location, images) {
-
+        
+        this.id = id
         this.name = name
         this.status = status
         this.species = species
@@ -13,4 +14,34 @@ class Characters {
         this.active = true
         
     }
+
+    toCharcterHtml(position) {
+        return `
+        <div class="character" onclick='characterSelected(${position})'>
+            <h2>${this.name}</h2>
+            <img 
+            height="120"
+            alt="character" 
+            src="${this.images[0]}"/>
+            <div class="images">
+                <img 
+            height="40"
+            alt="character" 
+            src="${this.images[0]}"/>
+            <img 
+            height="40"
+            alt="character" 
+            src="${this.images[1]}"/>
+            <img 
+            height="40"
+            alt="character" 
+            src="${this.images[2]}"/>
+            </div>
+            <label>${this.status}</label>
+            <p>${this.species}</p>
+            <label>$ ${this.gender}</label>
+        </div>
+        `
+    }
+
 }
