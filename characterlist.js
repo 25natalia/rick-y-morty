b8fd46fa39e32765ab63f543e6fd4d273923c6f7
kikipou <kta2122006@gmail.com>
2023-10-25 19:41:31 -0500

    demen acceso pls
    ayudaaa

---
+++ characterlist.js
@@ -0,0 +1,16 @@
+class Characters {
+
+    constructor(id, name, status, species, type, gender, origin, location, images) {
+
+        this.name = name
+        this.status = status
+        this.species = species
+        this.type = type
+        this.gender = gender
+        this.origin = origin
+        this.location = location
+        this.images = images
+        this.active = true
+        
+    }
+}
